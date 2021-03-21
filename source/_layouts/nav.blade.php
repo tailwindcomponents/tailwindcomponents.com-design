668242a6278f318dac92bc7306f7560eca08199e
khatabwedaa <khatabwedaa@gmail.com>
2021-03-21 13:37:51 +0200

    search page & Add dropdown

--- source/_layouts/nav.blade.php
+++ source/_layouts/nav.blade.php
@@ -1,6 +1,6 @@
 <div class="flex flex-col space-y-2 lg:space-y-0 lg:flex-row lg:space-x-6 xl:space-x-8 lg:items-center">
-    <a class="text-primary hover:underline" href="#">Home</a>
-    <a class="text-gray-500 hover:text-primary hover:underline" href="#">Components</a>
+    <a class="text-primary hover:underline" href="/">Home</a>
+    <a class="text-gray-500 hover:text-primary hover:underline" href="/components">Components</a>
     <a class="text-gray-500 hover:text-primary hover:underline" href="#">Featured</a>
     <a class="text-gray-500 hover:text-primary hover:underline" href="#">Blog</a>
     <a class="text-gray-500 hover:text-primary hover:underline" href="#">Cheatsheet</a>
@@ -16,9 +16,35 @@
     <a href="#" class="px-4 py-2 text-sm font-semibold text-center text-white rounded-md bg-primary hover:bg-teal-300">
         Submit new component
     </a>
-    
-    <button class="flex items-center justify-center space-x-2 focus:outline-none">
-        <img class="object-cover w-8 h-8 border-2 rounded-full border-primary lg:w-10 lg:h-10 " src="https://images.unsplash.com/photo-1502767882403-636aee14f873?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar">
-        <span class="font-medium text-gray-800 lg:hidden">Jane Doe</span>
-    </button>
+
+    <div class="relative" @mouseover="dropdown = true" @mouseover.away="dropdown = false"  x-data="{ dropdown: true }">
+        <!-- Dropdown toggle button -->
+        <button class="flex items-center justify-center space-x-2 focus:outline-none">
+            <img class="object-cover w-8 h-8 border-2 rounded-full border-primary lg:w-10 lg:h-10 " src="https://images.unsplash.com/photo-1502767882403-636aee14f873?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" alt="avatar">
+            <span class="font-medium text-gray-800 lg:hidden">Jane Doe</span>
+        </button>
+
+        <!-- Dropdown menu -->
+        <div
+            x-show="dropdown" 
+            x-transition:enter="transition ease-out duration-100 transform"
+            x-transition:enter-start="opacity-0 scale-95"
+            x-transition:enter-end="opacity-100 scale-100"
+            x-transition:leave="transition ease-in duration-75 transform"
+            x-transition:leave-start="opacity-100 scale-100"
+            x-transition:leave-end="opacity-0 scale-95"
+            class="absolute right-0 z-20 w-48 py-1 mt-2 bg-white border border-gray-100 rounded-md shadow-xl dark:bg-gray-800">
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-primary hover:text-white dark:hover:text-white">
+                your profile
+            </a>
+
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-primary hover:text-white dark:hover:text-white">
+                Settings
+            </a>
+            <hr>
+            <a href="#" class="block px-4 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 transform dark:text-gray-300 hover:bg-primary hover:text-white dark:hover:text-white">
+                Log Out
+            </a>
+        </div>
+    </div>
 </div>
